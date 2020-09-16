8b781ed88789322e13484ba552f1350188f8ea41
allezz <aerbay@gmail.com>
2020-09-16 02:24:51 +0200

    Setup App js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [data, setData] = useState({});
+
+  const chooseCountry = () => {
+    // data fetch
+  };
+
+  useEffect(() => {
+    // data fetch
+  }, []);
+
+  return (
+    <div className="App">
+      <Cards data={data} />
+      <SelectCountry country={selectedCountry} chooseCountry={chooseCountry} />
+      <Graph data={data} country={selectedCountry} />
+    </div>
+  );
 }
 
 export default App;
