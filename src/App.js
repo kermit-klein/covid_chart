d63fd4a89bcf48e6b228fcd562b83e10dfc15bb5
allezz <aerbay@gmail.com>
2020-09-17 03:02:48 +0200

    Add daily data and countries list api calls

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import getData from "./modules/api";
 
 function App() {
   const [selectedCountry, setSelectedCountry] = useState("");
--- src/modules/api.js
+++ src/modules/api.js
@@ -1,12 +1,34 @@
 import axios from "axios";
 
 const getData = async (country) => {
+  const url = country
+    ? `https://covid19.mathro.id/api/countries/${country}`
+    : "https://covid19.mathro.id/api/";
+
   try {
-    let response = await axios.get(
-      `https://covid19.mathro.id/api/countries/${country}`
-    );
+    let response = await axios.get(url);
     return response.data;
   } catch (err) {
     console.log(err);
   }
 };
+
+const getDailyData = async () => {
+  try {
+    let response = await axios.get("https://covid19.mathro.id/api/daily");
+    return response.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const allCountries = () => {
+    try {
+        let response = await axios.get("https://covid19.mathro.id/api/countries")
+        return response.data
+    } catch (error) {
+        console.log(err)
+    }
+}
+
+export { getData, getDailyData, allCountries };
