52dc5662385040605d68780696ea42c2b2e7010b
allezz <aerbay@gmail.com>
2020-09-18 06:54:20 +0200

    Create css module files, add image to main page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import getData from "./modules/api";
 import classes from "./stylesheets/App.module.scss";
+import { Graph, SelectCountry, Cards } from "./components/";
+import covid from "./images/covid1.jpg";
 
 function App() {
   const [selectedCountry, setSelectedCountry] = useState("");
@@ -19,6 +21,7 @@ function App() {
 
   return (
     <div className={classes.container}>
+      <img className={classes.image} src={covid} alt="Covid" />
       <Cards data={data} />
       <SelectCountry country={selectedCountry} chooseCountry={chooseCountry} />
       <Graph data={data} country={selectedCountry} />
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,4 +1,14 @@
 import React from "react";
+import {
+  CircularProgress,
+  Card,
+  CardContent,
+  Typography,
+  Grid,
+} from "@material-ui/core";
+import CountUp from "react-countup";
+import classes from "../stylesheets/Cards.module.scss";
+import clx from "classnames";
 
 const Cards = () => {
   return <div></div>;
