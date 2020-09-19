33fa2378f0a5c7f075b8fa1d9c0f92f2e1a58635
allezz <aerbay@gmail.com>
2020-09-19 21:57:47 +0200

    Add select country html elements

--- src/components/SelectCountry.js
+++ src/components/SelectCountry.js
@@ -19,7 +19,36 @@ const SelectCountry = (props) => {
     })();
   }, []);
 
-  return <div></div>;
+  const pickCountry = (e) => {
+    setCountry(e.target.value);
+    props.chooseCountry(e.target.value === "Global" ? "" : e.target.value);
+  };
+
+  if (!countryList.length) {
+    return <CircularProgress></CircularProgress>;
+  }
+
+  return (
+    <FormControl variant="outlined" className={classes.formcontrol}>
+      <InputLabel className={classes.label} id="country">
+        Select Country
+      </InputLabel>
+      <Select
+        labelId="country-picker"
+        id="country-pick"
+        label="Select Country"
+        value={country}
+        onChange={pickCountry}
+      >
+        <MenuItem value="Global">Global</MenuItem>
+        {countryList.map((name, index) => {
+          <MenuItem key={index} value={name}>
+            {name}
+          </MenuItem>;
+        })}
+      </Select>
+    </FormControl>
+  );
 };
 
 export default SelectCountry;
