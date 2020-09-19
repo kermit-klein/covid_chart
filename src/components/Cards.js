33448afd73b95bb58878a2def21c864ede13a90e
allezz <aerbay@gmail.com>
2020-09-19 06:06:14 +0200

    Add recovered html elements

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -22,23 +22,57 @@ const Cards = (props) => {
   return (
     <div className={classes.container}>
       <Grid container spacing={3} justify="center">
-        <CardContent>
-          <Typography color="textSecondary" gutterBottom>
-            Infected
-          </Typography>
-          <Typography variant="h5">
-            <CountUp
-              start={0}
-              end={data.confirmed}
-              duration={2.5}
-              separator=","
-            />
-          </Typography>
-          <Typography color="textSecondary">
-            {new Date(data.lastDate).toDateString()}
-          </Typography>
-          <Typography variant="body2">Number of Active Cases</Typography>
-        </CardContent>
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={clx(classes.infected, classes.card)}
+        >
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={data.confirmed}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(data.lastDate).toDateString()}
+            </Typography>
+            <Typography variant="body2">Number of Active Cases</Typography>
+          </CardContent>
+        </Grid>
+
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={clx(classes.recovered, classes.card)}
+        >
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Recovered
+            </Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={data.recovered}
+                duration={2.5}
+                separator=","
+              />
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(data.lastDate).toDateString()}
+            </Typography>
+            <Typography variant="body2">Number of Recovered</Typography>
+          </CardContent>
+        </Grid>
       </Grid>
     </div>
   );
