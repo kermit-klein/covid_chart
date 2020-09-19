acb64796ec25bcabe1ba242db4f7ac69236d769b
allezz <aerbay@gmail.com>
2020-09-19 21:07:58 +0200

    Add style select country

--- src/components/SelectCountry.js
+++ src/components/SelectCountry.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  CircularProgress,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+} from "@material-ui/core";
+import { allCountries } from "../modules/api.js";
+import classes from "../stylesheets/SelectCountry.module.scss";
 
-const SelectCountry = () => {
+const SelectCountry = (props) => {
+  const [countryList, setCountryList] = useState([]);
+  const [country, setCountry] = useState("Global");
+
+  useEffect(() => {
+    (async () => {
+      setCountryList(await allCountries());
+    })();
+  }, []);
+
   return <div></div>;
 };
 
