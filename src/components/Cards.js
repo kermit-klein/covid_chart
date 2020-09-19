c246a6b6ca81319064cfcc22bec56312794f6daf
allezz <aerbay@gmail.com>
2020-09-19 05:56:41 +0200

    Add html elements for infected

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -10,8 +10,38 @@ import CountUp from "react-countup";
 import classes from "../stylesheets/Cards.module.scss";
 import clx from "classnames";
 
-const Cards = () => {
-  return <div></div>;
+const Cards = (props) => {
+  if (!props.data.confirmed) {
+    return (
+      <div>
+        <CircularProgress />
+      </div>
+    );
+  }
+
+  return (
+    <div className={classes.container}>
+      <Grid container spacing={3} justify="center">
+        <CardContent>
+          <Typography color="textSecondary" gutterBottom>
+            Infected
+          </Typography>
+          <Typography variant="h5">
+            <CountUp
+              start={0}
+              end={data.confirmed}
+              duration={2.5}
+              separator=","
+            />
+          </Typography>
+          <Typography color="textSecondary">
+            {new Date(data.lastDate).toDateString()}
+          </Typography>
+          <Typography variant="body2">Number of Active Cases</Typography>
+        </CardContent>
+      </Grid>
+    </div>
+  );
 };
 
 export default Cards;
